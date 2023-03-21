d867636847ba11dac269977db7a54ac8edf8817d
Elvan Hidayat <elvanhd@gmail.com>
2023-03-21 19:06:13 +0700

    create AccountController with register endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+    private readonly ITokenService _tokenService;
+
+    public AccountController(DataContext context, ITokenService tokenService)
+    {
+        _tokenService = tokenService;
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username))
+        {
+            return BadRequest("Username is taken");
+        }
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(CultureInfo.CurrentCulture),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _ = _context.Users.Add(user);
+        _ = await _context.SaveChangesAsync();
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower(CultureInfo.CurrentCulture));
+    }
+}
