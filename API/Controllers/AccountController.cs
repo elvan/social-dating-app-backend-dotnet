30ea10d95af79b6df19af35088d4411e487476aa
Elvan Hidayat <elvanhd@gmail.com>
2023-03-21 19:22:18 +0700

    add a login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -51,6 +51,35 @@ public class AccountController : BaseApiController
         };
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+        if (user == null)
+        {
+            return Unauthorized("invalid username");
+        }
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (var i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("invalid password");
+            }
+        }
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
     private async Task<bool> UserExists(string username)
     {
         return await _context.Users.AnyAsync(x => x.UserName == username.ToLower(CultureInfo.CurrentCulture));
