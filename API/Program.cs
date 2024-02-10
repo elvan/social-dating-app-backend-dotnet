116a9dd0bd4a1b92021e5d7cd4527e3a99ebb2c0
Elvan Hidayat <elvanhd@gmail.com>
2024-02-11 04:26:21 +0700

    Enable CORS for frontend integration
    - Add CORS services to the application
    - Configure CORS policy to allow any header, method, and specific origin

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
