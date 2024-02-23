93cbf56d9670a087ad54b7cc2e3b06cf951aa6ef
Elvan Hidayat <elvanhd@gmail.com>
2024-02-23 21:57:24 +0700

    Add user profile update functionality
    - Implement UpdateUser action in UsersController to allow user profile updates
    - Introduce MemberUpdateDto for capturing user updates
    - Extend AutoMapperProfiles to map MemberUpdateDto to AppUser entity
    - Utilize ClaimTypes.NameIdentifier to identify the current user for updates
    - Ensure successful updates return NoContent, and failures return BadRequest

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using API.Interfaces;
+﻿using System.Security.Claims;
+
+using API.DTOs;
+using API.Interfaces;
 
 using AutoMapper;
 
@@ -31,5 +34,20 @@ public class UsersController : BaseApiController
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         return await _userRepository.GetMemberAsync(username);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        _mapper.Map(memberUpdateDto, user);
+
+        _userRepository.Update(user);
+
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update user");
     }
 }
