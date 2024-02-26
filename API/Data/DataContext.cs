3c3b0c333e716fab29f06d8e6a0367a9fb3a3684
Elvan Hidayat <elvanhd@gmail.com>
2024-02-26 23:14:43 +0700

    Implement messaging feature for user communication
    - Add MessagesController with actions to send, retrieve, and delete messages
    - Create Message entity and update DataContext for message handling
    - Implement MessageRepository for message-related database operations
    - Add DTOs for creating and displaying messages (CreateMessageDto, MessageDto)
    - Configure AutoMapper profiles for message mapping
    - Introduce MessageParams and extend PaginationParams for message filtering
    - Update ApplicationServiceExtensions to inject IMessageRepository
    - Modify AppUser entity to include navigation properties for sent and received messages
    - Implement migrations to add the Message entity to the database

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -12,6 +12,7 @@ public class DataContext : DbContext
 
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -31,5 +32,15 @@ public class DataContext : DbContext
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Message>()
+            .HasOne(u => u.Sender)
+            .WithMany(m => m.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
