7c6d5a0f7194b9834f4b87d1cb4fdb32b9d19751
Elvan Hidayat <elvanhd@gmail.com>
2024-02-28 21:34:29 +0700

    Add SignalR for live chat functionality
    - Implement MessageHub for handling real-time chat operations
    - Add PresenceTracker to keep track of online users
    - Update MessageRepository with methods for managing chat groups and connections
    - Create new entities: Group and Connection
    - Update DataContext and run migration to reflect changes in the database
    - Modify related files to integrate the new chat feature

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -21,6 +21,11 @@ public class MessageRepository : IMessageRepository
         _mapper = mapper;
     }
 
+    public void AddGroup(Group group)
+    {
+        _context.Groups.Add(group);
+    }
+
     public void AddMessage(Message message)
     {
         _context.Messages.Add(message);
@@ -31,9 +36,29 @@ public class MessageRepository : IMessageRepository
         _context.Messages.Remove(message);
     }
 
+    public async Task<Connection> GetConnection(string connectionId)
+    {
+        return await _context.Connections.FindAsync(connectionId);
+    }
+
+    public async Task<Group> GetGroupForConnection(string connectionId)
+    {
+        return await _context.Groups
+            .Include(x => x.Connections)
+            .Where(x => x.Connections.Any(c => c.ConnectionId == connectionId))
+            .FirstOrDefaultAsync();
+    }
+
     public async Task<Message> GetMessage(int id)
     {
         return await _context.Messages.FindAsync(id);
+    }
+
+    public async Task<Group> GetMessageGroup(string groupName)
+    {
+        return await _context.Groups
+            .Include(x => x.Connections)
+            .FirstOrDefaultAsync(x => x.Name == groupName);
     }
 
     public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -60,10 +85,8 @@ public class MessageRepository : IMessageRepository
             .Include(u => u.Sender).ThenInclude(p => p.Photos)
             .Include(u => u.Recipient).ThenInclude(p => p.Photos)
             .Where(
-                m => m.RecipientUsername == currentUsername && m.RecipientDeleted == false &&
-                    m.SenderUsername == recipientUsername ||
-                    m.RecipientUsername == recipientUsername &&
-                    m.SenderUsername == currentUsername && m.SenderDeleted == false
+                m => m.RecipientUsername == currentUsername && m.RecipientDeleted == false && m.SenderUsername == recipientUsername ||
+                    m.RecipientUsername == recipientUsername && m.SenderUsername == currentUsername && m.SenderDeleted == false
             )
             .OrderBy(m => m.MessageSent)
             .ToListAsync();
@@ -81,6 +104,11 @@ public class MessageRepository : IMessageRepository
         }
 
         return _mapper.Map<IEnumerable<MessageDto>>(messages);
+    }
+
+    public void RemoveConnection(Connection connection)
+    {
+        _context.Connections.Remove(connection);
     }
 
     public async Task<bool> SaveAllAsync()
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,9 @@
+using API;
 using API.Data;
 using API.Entities;
 using API.Errors;
 using API.Extensions;
+using API.SignalR;
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -31,7 +33,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
@@ -39,6 +45,8 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<PresenceHub>("hubs/presence");
+app.MapHub<MessageHub>("hubs/message");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -49,6 +57,7 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
+    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
