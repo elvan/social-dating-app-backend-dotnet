975154a3bd4c9b641977600db4f1b759394ac042
Elvan Hidayat <elvanhd@gmail.com>
2024-08-25 06:36:54 +0700

    Implement user activity logging and pagination features
    - Add user activity logging with LogUserActivity filter
    - Implement pagination in user listing with PagedList helper
    - Extend UserDto and claims to include Gender information
    - Update UserRepository to support filtering and sorting by age, gender
    - Add HttpExtensions for pagination headers in API responses
    - Include UserParams for handling pagination and filtering in queries
    - Enhance TokenService to include user ID in JWT claims

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -36,7 +36,8 @@ public class AccountController(DataContext context, ITokenService tokenService,
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user),
-            KnownAs = user.KnownAs
+            KnownAs = user.KnownAs,
+            Gender = user.Gender
         };
     }
 
@@ -64,6 +65,7 @@ public class AccountController(DataContext context, ITokenService tokenService,
             Username = user.UserName,
             KnownAs = user.KnownAs,
             Token = tokenService.CreateToken(user),
+            Gender = user.Gender,
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
     }
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 
 using AutoMapper;
@@ -15,9 +16,12 @@ public class UsersController(IUserRepository userRepository, IMapper mapper,
     IPhotoService photoService) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
     {
-        var users = await userRepository.GetMembersAsync();
+        userParams.CurrentUsername = User.GetUsername();
+        var users = await userRepository.GetMembersAsync(userParams);
+
+        Response.AddPaginationHeader(users);
 
         return Ok(users);
     }
